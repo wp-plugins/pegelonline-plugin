d1bb7e18d92a3cc1c2bf6ff3a97e13ad7468435f
daburna <daburna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-31 13:40:32 +0000

    release 0.0.4
    git-svn-id: https://plugins.svn.wordpress.org/pegelonline-plugin/tags/0.0.4@141387 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pegelonline.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: PegelOnline-Plugin
+Plugin URI: http://www.daburna.de/blog/2008/12/30/pegelonline-plugin/
+Description: Bindet den Pegelstand von Gewässern in deinen Blog ein.
+Version: 0.0.4
+Author: Oliver Wunder 
+Author URI: http://www.daburna.de/
+
+
+*/
+
+$ow_pegelonline_nr_field = get_option('ow_pegelonline_nr_field');
+$ow_pegelonline_titel_field = get_option('ow_pegelonline_titel_field');
+$ow_pegelonline_imgwidth_field = get_option('ow_pegelonline_imgwidth_field');
+$ow_pegelonline_imgheight_field = get_option('ow_pegelonline_imgheight_field');
+$ow_pegelonline_objwidth_field = get_option('ow_pegelonline_objwidth_field');
+$ow_pegelonline_objheight_field = get_option('ow_pegelonline_objheight_field');
+ 
+if ('insert' == $HTTP_POST_VARS['action'])
+{
+    update_option("ow_pegelonline_nr_field",$HTTP_POST_VARS['ow_pegelonline_nr_field']);
+    update_option("ow_pegelonline_titel_field",$HTTP_POST_VARS['ow_pegelonline_titel_field']);
+    update_option("ow_pegelonline_imgwidth_field",$HTTP_POST_VARS['ow_pegelonline_imgwidth_field']);
+    update_option("ow_pegelonline_imgheight_field",$HTTP_POST_VARS['ow_pegelonline_imgheight_field']);    
+    update_option("ow_pegelonline_objwidth_field",$HTTP_POST_VARS['ow_pegelonline_objwidth_field']);
+    update_option("ow_pegelonline_objheight_field",$HTTP_POST_VARS['ow_pegelonline_objheight_field']);
+}
+ 
+// Innerhalb von the_loop reicht das
+
+function ow_pegelonline() {
+
+	?>
+	<?=get_option("ow_pegelonline_titel_field");?>
+	<object type="text/html" data="http://www.pegelonline.wsv.de/charts/OnlineVisualisierungGanglinie?pegelnummer=<?=get_option("ow_pegelonline_nr_field");?>&imgBreite=<?=get_option("ow_pegelonline_imgwidth_field");?>&imgHoehe=<?=get_option("ow_pegelonline_imgheight_field");?>&schriftPegelname=6&schriftLetzterWert=6&anzeigeUeberschrift=false&textUnten=" style="height:<?=get_option("ow_pegelonline_objheight_field");?>px; width:<?=get_option("ow_pegelonline_objwidth_field");?>px;"></object>       
+	<?php
+
+} // Ende Funktion ow_pegelonline()
+ 
+function ow_pegelonline_option_page() {
+?>
+ 
+<!-- Start Optionen im Adminbereich (xhtml) -->
+  <div class="wrap">
+    <h2>PegelOnline-Plugin Optionen</h2>
+    <p>
+    Dies ist eine frühe Version des PegelOnline Plugins. Hiermit lassen sich aktuelle Pegelstände von Gewässern im Blog einbinden. Die Daten stammen von dem
+    gewässerkundlichen Informationssystem <a href="http://www.pegelonline.wsv.de/" title="PEGELONLINE">PEGELONLINE</a>.
+    Um das Plugin nutzen zu können, mußt du als erstes eine Pegelnummer eingeben. Diese findest du in der <a href="http://www.pegelonline.wsv.de/gast/pegelinformationen" title="Liste aller verfügbaren Pegel">Liste aller verfügbaren Pegel</a>.
+    </p>
+    <p>
+    Mit dem folgendem Befehl kannst du dann den aktuellen Pegelstand eines ausgewählten Gewässers in deinem Blog einbinden: <strong>&lt;?php if (function_exists('ow_pegelonline')) {  ow_pegelonline();} ?&gt</strong>    
+    </p>
+    <h3>Bekannte Bugs</h3>
+    <p>
+    	# Benutzer des Browsers Opera können Darstellungsprobleme haben, da die kleinste darstellbare Schriftgröße automatisch auf 9px festgelegt ist. Hier im Plugin kann sie aber auch kleiner gewählt werden. 
+    	Um das Problem zu beheben muss in Opera folgendes getan werden: Einstellungen - Erweitert - Schriften - kleinste Schriftgröße (in Pixel) von 9 auf 6 umstellen.   
+    </p>
+    <form name="form1" method="post" action="<?=$location ?>">
+    <table>
+      <tr>
+        <td><label for="ow_pegelonline_nr_field">Pegelnummer:</label></td><td><input name="ow_pegelonline_nr_field" value="<?=get_option("ow_pegelonline_nr_field");?>" type="text" id="ow_pegelonline_nr_field" /> </td>
+      </tr>
+		<tr>
+		  <td><label for="ow_pegelonline_titel_field">Überschrift:</label></td><td><input name="ow_pegelonline_titel_field" value="<?=get_option("ow_pegelonline_titel_field");?>" type="text" id="ow_pegelonline_titel_field" /></td>
+		</tr>
+		<tr>
+		  <td><label for="ow_pegelonline_imgwidth_field">Breite der Grafik:</label></td><td><input name="ow_pegelonline_imgwidth_field" value="<?=get_option("ow_pegelonline_imgwidth_field");?>" type="text" id="ow_pegelonline_imgwidth_field" /></td>
+		</tr>
+		<td><label for="ow_pegelonline_imgheight_field">Höhe der Grafik:</label></td><td><input name="ow_pegelonline_imgheight_field" value="<?=get_option("ow_pegelonline_imgheight_field");?>" type="text" id="ow_pegelonline_imgheight_field" /></td>
+		</tr>
+		<tr>
+		  <td><label for="ow_pegelonline_objwidth_field">Breite des Objekts:</label></td><td><input name="ow_pegelonline_objwidth_field" value="<?=get_option("ow_pegelonline_objwidth_field");?>" type="text" id="ow_pegelonline_objwidth_field" /></td>
+		</tr>
+		<td><label for="ow_pegelonline_objheight_field">Höhe des Objekts:</label></td><td><input name="ow_pegelonline_objheight_field" value="<?=get_option("ow_pegelonline_objheight_field");?>" type="text" id="ow_pegelonline_objheight_field" /></td>
+		</tr>
+ 		     
+      <tr>
+        <td><input type="submit" class="button-primary" value="Speichern" />
+        <input name="action" value="insert" type="hidden" /></td>
+      </tr>
+    </table>
+    </form>
+    <h3>Vorschau:</h3>
+    <div style="border:1px solid;">
+    <?php ow_pegelonline(); ?>
+	 </div>    
+  </div>
+<!-- ENDE Optionen im Adminbereich (xhtml) -->
+
+<?php
+} // Ende Funktion ow_pegelonline_option_page()
+ 
+// Adminmenu Optionen erweitern
+function ow_pegelonline_add_menu() {
+  add_option("ow_pegelonline_nr_field","9650072"); // optionsfield in Tabelle TABLEPRÄFIX_options
+  add_option("ow_pegelonline_titel_field","<h3>Pegelstand</h3>");
+  add_option("ow_pegelonline_imgwidth_field","150");
+  add_option("ow_pegelonline_imgheight_field","200");
+  add_option("ow_pegelonline_objwidth_field","230");
+  add_option("ow_pegelonline_objheight_field","300");    
+  add_options_page('PegelOnline-Plugin', 'PegelOnline', 9, __FILE__, 'ow_pegelonline_option_page'); //optionenseite hinzufügen
+}
+ 
+// Registrieren der WordPress-Hooks
+add_action('admin_menu', 'ow_pegelonline_add_menu');
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== PegelOnline-Plugin ===
+Contributors: daburna
+Donate link: https://www.paypal.com/de/cgi-bin/webscr?cmd=_flow&SESSION=HIThLCeNy6Ejvz8Fp0E3nBCmHfwYLNQaZn9I1Q6nVxQVSNisNxeRpIzkiAK&dispatch=5885d80a13c0db1fb6947b0aeae66fdb090c3508df63c7a078883ba93cc8aafe
+Tags: embedding, water level, water, water gage, GIS, Pegel, Gewässer, Fluß, Fluss, Meer, Hydrologie, hydrology, Geographie, geography, Wasser, monitoring
+Requires at least: 2.0.0
+Tested up to: 2.8
+Stable tag: 0.0.4
+
+== Description ==
+
+A plugin for monitoring the water level of e.g. a river into your blog. Works only with water level in Germany, so the following instructions 
+are in German.
+
+Das PegelOnline-Plugin bindet den Pegelstand von Gewässern im Blog ein. Die Daten stammen von gewässerkundlichen Informationssystem der Wasser- 
+und Schifffahrtsverwaltung des Bundes.
+
+== Installation ==
+
+Die Datei pegelonline.php in den wp-content/plugins Ordner ihrer WordPress-Installation entpacken. Danach das Plugin aktivieren. Im Menü Einstellungen 
+können nun Parameter für das PegelOnline-Plugin eingegeben werden.
+
+== Using the plugin ==
+
+An einer beliebigen Stelle im Blog <?php if (function_exists('ow_pegelonline')) { ow_pegelonline();} ?> einsetzen.
+
+
+== Changelog ==
+
+= 0.0.4 - 31.07.2009 =
+* Infotext überarbeitet
++ Hinweis aus Probleme mit Opera
++ Changelog hinzugefügt
+
+= 0.0.3 - 07.01.2009 =
++ Breite und Höhe konfigurierbar
+
+= 0.0.2 - 31.12.2008 =
++ Vorschau
++ Überschrift
++ Optionen in Tabellenform
+
+= 0.0.1 - 30.12.2008 =
++ Initialzündung
